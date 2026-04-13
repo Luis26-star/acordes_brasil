bbbfa526938ab888d7b3e5dd5b7f5e52c82329f2
Luis26-star <fraglu@yahoo.com.br>
2026-04-13 15:26:59 +0200

    Update repertoire.js

--- js/repertoire.js
+++ js/repertoire.js
@@ -1,1 +1,22 @@
+async function loadSongs(){
 
+const response = await fetch("data/songs.json")
+const songs = await response.json()
+
+const container = document.getElementById("songs")
+
+songs.forEach(song => {
+
+const div = document.createElement("div")
+
+div.innerHTML =
+"<h3>" + song.title + "</h3>" +
+"<a href='song.html?s=" + song.folder + "'>▶ Play</a>"
+
+container.appendChild(div)
+
+})
+
+}
+
+loadSongs()
