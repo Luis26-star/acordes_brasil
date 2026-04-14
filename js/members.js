e1165da6c11ccb09c5e56a889c91a6cbd6189c7c
Luis26-star <fraglu@yahoo.com.br>
2026-04-14 22:40:21 +0200

    Update members.js

--- js/members.js
+++ js/members.js
@@ -1,29 +1,44 @@
+let allMembers = []
+
 fetch("data/members.json")
   .then(res => res.json())
   .then(members => {
+    allMembers = members
+    showMembers(members)
+  })
 
-    const container = document.getElementById("membersList")
+function showMembers(members) {
+  const container = document.getElementById("membersList")
+  container.innerHTML = ""
 
-    members.forEach(member => {
+  members.forEach(member => {
 
-      const div = document.createElement("div")
+    const div = document.createElement("div")
 
-      let colorClass = ""
+    let colorClass = ""
 
-      if (member.voice === "Sopran") colorClass = "sopran"
-      if (member.voice === "Alt") colorClass = "alt"
-      if (member.voice === "Tenor") colorClass = "tenor"
-      if (member.voice === "Bass") colorClass = "bass"
+    if (member.voice === "Sopran") colorClass = "sopran"
+    if (member.voice === "Alt") colorClass = "alt"
+    if (member.voice === "Tenor") colorClass = "tenor"
+    if (member.voice === "Bass") colorClass = "bass"
 
-      div.className = "member " + colorClass
+    div.className = "member " + colorClass
 
-      div.innerHTML = `
-        <strong>${member.name}</strong><br>
-        ${member.voice}
-      `
+    div.innerHTML = `
+      <strong>${member.name}</strong><br>
+      ${member.voice}
+    `
 
-      container.appendChild(div)
-
-    })
+    container.appendChild(div)
 
   })
+}
+
+function filterVoice(voice) {
+  if (voice === "all") {
+    showMembers(allMembers)
+  } else {
+    const filtered = allMembers.filter(m => m.voice === voice)
+    showMembers(filtered)
+  }
+}
