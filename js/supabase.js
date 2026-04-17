7b0ea098cdb8bc2027ce671f8a06bdbf672ad4b5
Luis26-star <fraglu@yahoo.com.br>
2026-04-17 17:40:03 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -4,7 +4,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // DEINE SUPABASE-DATEN (aus dem Dashboard)
 const SUPABASE_URL = "https://xpdettowaygoekrdxdpq.supabase.co"
-const SUPABASE_KEY = "HIER KOMMT DEIN ANON KEY REIN"  // ← Den musst du kopieren!
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhwZGV0dG93YXlnb2VrcmR4ZHBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0MzE0NjksImV4cCI6MjA5MjAwNzQ2OX0.gWkqyDcJ58qc-RvbquvTOCda7O5Tr7FsNTF9Bce451Q"  // ← Den musst du kopieren!
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
